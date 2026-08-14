ec3c9e1c950f293f5482f1a246e9b82b1784423d
KartikTyagi88 <kartiktyagi51kt@gmail.com>
2026-08-14 20:00:05 +0530

    Preventing samesite JWT token based verification

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,7 +8,7 @@ export const generateToken = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 7*24*60*60*1000,
         httpOnly: true,              // prevents XSS attacks
-        sameSite: "Strict",         // prevents CSRF attacks
+        sameSite: "None",           
         secure: process.env.NODE_ENV !== "development"
     });
 
