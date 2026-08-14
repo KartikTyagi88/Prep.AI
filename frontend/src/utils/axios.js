f64703e79e4c6d9cde020560e8f856ac79694f99
KartikTyagi88 <kartiktyagi51kt@gmail.com>
2026-08-14 16:12:13 +0530

    Changing the baseURL to match render URL

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: "https://prep-ai-1qnq.onrender.com",
     withCredentials: true, 
 });
 
