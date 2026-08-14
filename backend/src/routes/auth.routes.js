13d084aab78aa702d178eb01ac4d2bef33d5c082
KartikTyagi88 <kartiktyagi51kt@gmail.com>
2026-08-15 00:56:46 +0530

    Fixing bugs

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { login, logout, signup, getProfile } from "../controllers/auth.controller.js";
+import { login, logout, signup, getProfile, updateName } from "../controllers/auth.controller.js";
 import {protectRoute} from "../middlewares/auth.middleware.js"
 const router = express.Router();
 
